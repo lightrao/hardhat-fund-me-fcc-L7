d1941f917347e37d6afe118e34eb62f7b2fdea16
RAO YANG <lightrao@hotmail.com>
2022-08-24 19:58:54 +0800

    Solidity Style Guide

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,49 +1,74 @@
 // SPDX-License-Identifier: MIT
+// Pragam
 pragma solidity ^0.8.8;
-
+// imports
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
+// Error codes
+error FundMe__NotOwner();
 
-error NotOwner();
+// Interfaces, Libraries, Contracts
 
+/** @title A contract for crowd fundding
+ *  @author Light Rao
+ *  @notice This contract is to demao a simple fundding contract
+ *  @dev This implements price feeds as our library
+ */
 contract FundMe {
+    // 1.Type declarations
     using PriceConverter for uint256;
-
+    // 2.State variables
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public immutable i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10**18;
-
     AggregatorV3Interface public priceFeed;
+    // 3.Events
+    // _;
+    // 4.Modifiers
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
 
+    // 5.Functions
+    /// constructor
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
+    /// receive
+    receive() external payable {
+        fund();
+    }
+
+    /// fallback
+    fallback() external payable {
+        fund();
+    }
+
+    /// external
+    /// _;
+    /// public
+
+    /**
+     *  @notice This function funds this contract
+     *  @dev This implements price feeds as our library
+     */
     function fund() public payable {
         require(
             msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
-        // AggregatorV3Interface priceFeed = AggregatorV3Interface(
-        //     0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-        // );
         return priceFeed.version();
-    }
-
-    modifier onlyOwner() {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
     }
 
     function withdraw() public payable onlyOwner {
@@ -56,23 +81,15 @@ contract FundMe {
             addressToAmountFunded[funder] = 0; // reset map structure's value to 0
         }
         funders = new address[](0); // funders point to recreated array which include 0 items
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-        // call
+
         (bool callSuccess, ) = payable(msg.sender).call{
             value: address(this).balance
         }("");
         require(callSuccess, "Call failed");
     }
+    /// internal
 
-    fallback() external payable {
-        fund();
-    }
+    /// private
 
-    receive() external payable {
-        fund();
-    }
+    /// view / pure
 }
